3f89c48adeb54f4420b0b66f6080949bc2840258
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2020-05-30 22:24:03 -0400

    default font open sans adjustments

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans&display=swap' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap' }
     ]
   },
   /*
