d549c3dfd4e8179ad4acc94ba16e0ff9414f967d
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2021-08-23 17:10:16 -0400

    default sort is date published desc

--- store/state.js
+++ store/state.js
@@ -13,10 +13,10 @@ export default () => ({
   },
   filter: {},
   sortOrderOptions: [
-    {id: 0, byField: 'dateAdded', direction: 'asc', en: "date posted (new to old)", fr: "date postée (nouveau à vieux)"},
-    {id: 1, byField: 'dateAdded', direction: 'desc', en: "date posted (old to new)", fr: "date postée (ancien au nouveau)"},
-    {id: 2, byField: 'datePublished', direction: 'desc', en: "date published (new to old)", fr: "date publiée (nouveau à vieux)"},
-    {id: 3, byField: 'datePublished', direction: 'asc', en: "date published (old to new)", fr: "date publiée (ancien au nouveau)"}
+    {id: 0, byField: 'datePublished', direction: 'desc', en: "date published (new to old)", fr: "date publiée (nouveau à vieux)"},
+    {id: 1, byField: 'datePublished', direction: 'asc', en: "date published (old to new)", fr: "date publiée (ancien au nouveau)"},
+    {id: 2, byField: 'dateAdded', direction: 'asc', en: "date posted (new to old)", fr: "date postée (nouveau à vieux)"},
+    {id: 3, byField: 'dateAdded', direction: 'desc', en: "date posted (old to new)", fr: "date postée (ancien au nouveau)"}
   ],
   sortOrderId: 0,
   copy: [],
