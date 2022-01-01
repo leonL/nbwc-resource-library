2d42ac63e86f0992fdf2b1c7cb12dd457bbb56fc
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2022-01-01 15:54:56 -0500

    correct sortOrderOption mismatch

--- store/state.js
+++ store/state.js
@@ -6,8 +6,8 @@ export default () => ({
   sortOrderOptions: [
     {id: 0, byField: 'datePublished', direction: 'desc', en: "date published (new to old)", fr: "date publiée (nouveau à vieux)"},
     {id: 1, byField: 'datePublished', direction: 'asc', en: "date published (old to new)", fr: "date publiée (ancien au nouveau)"},
-    {id: 2, byField: 'dateAdded', direction: 'asc', en: "date posted (new to old)", fr: "date postée (nouveau à vieux)"},
-    {id: 3, byField: 'dateAdded', direction: 'desc', en: "date posted (old to new)", fr: "date postée (ancien au nouveau)"}
+    {id: 2, byField: 'dateAdded', direction: 'desc', en: "date posted (new to old)", fr: "date postée (nouveau à vieux)"},
+    {id: 3, byField: 'dateAdded', direction: 'asc', en: "date posted (old to new)", fr: "date postée (ancien au nouveau)"}
   ],
   sortOrderId: 0,
   copy: [],
